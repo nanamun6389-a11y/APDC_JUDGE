64694e8fa94aa3ffa88161da1faa1b728f80cb54
nanamun6389-a11y <nanamun6389@gmail.com>
2026-08-18 16:45:17 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'apdc-v2-judge-20260818-cert-clean-v12';
+const CACHE_NAME = 'apdc-v2-judge-20260818-cert-image-v13';
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch', e => { if (e.request.method === 'GET') e.respondWith(fetch(e.request)); });
